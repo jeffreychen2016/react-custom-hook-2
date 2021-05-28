7b43e22a9e85c2c17a41dfe134a805cfe92d235b
JeffreyChen <jeffreychen@JeffreyChens-MacBook-Pro.local>
2021-05-28 17:57:18 -0500

    chore: add project

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  // OLD WAY
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-http-6b4a6.firebaseio.com/tasks.json"
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const trasnformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: "https://react-http-6b4a6.firebaseio.com/tasks.json" },
+      trasnformTasks
+    );
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
